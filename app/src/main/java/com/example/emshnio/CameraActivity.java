40649b9941b8ef4be191042e777a00b4e0ec1fad
Balu Nathan <balu.nathan.04@gmail.com>
2019-03-17 13:13:43 -0400

    WIP:App does not crash, tf model does not return proper output.

---
+++ app/src/main/java/com/example/emshnio/CameraActivity.java
@@ -0,0 +1,323 @@
+package com.example.emshnio;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.otaliastudios.cameraview.AspectRatio;
+import com.otaliastudios.cameraview.CameraException;
+import com.otaliastudios.cameraview.CameraListener;
+import com.otaliastudios.cameraview.CameraLogger;
+import com.otaliastudios.cameraview.CameraOptions;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.Mode;
+import com.otaliastudios.cameraview.PictureResult;
+import com.otaliastudios.cameraview.SizeSelector;
+import com.otaliastudios.cameraview.SizeSelectors;
+import com.otaliastudios.cameraview.VideoResult;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+import androidx.exifinterface.media.*;
+
+
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener, ControlView.Callback {
+
+    private CameraView camera;
+    private ViewGroup controlPanel;
+
+    // To show stuff in the callback
+    private long mCaptureTime;
+
+
+    private MappedByteBuffer loadModelFile() throws IOException {
+        AssetFileDescriptor fileDescriptor = this.getAssets().openFd("model.tflite");
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    private float[][] doInference(PictureResult pictureResult) {
+
+        Interpreter tflite;
+        Bitmap netInput = BitmapFactory.decodeByteArray(pictureResult.getData(), 0, pictureResult.getData().length);
+        float [][] output = new float[1][7];
+
+        netInput = Bitmap.createScaledBitmap(netInput, 200, 200, false);
+
+        try {
+            tflite = new Interpreter(loadModelFile());
+            InputStream is = this.getAssets().open("nikola_happy.jpg");
+            netInput = BitmapFactory.decodeStream(is);
+
+
+            float [][][][] img = new float[1][200][200][3];
+            for (int i = 0; i < 200; i++){
+                for (int j = 0; j < 200; j++){
+                    int p = netInput.getPixel(j, i);
+                    img[0][i][j][0] = ((p >> 16) & 0xff) / (float)255;
+                    img[0][i][j][1] = ((p >> 8) & 0xff) / (float)255;
+                    img[0][i][j][2] = (p & 0xff) / (float)255;
+                }
+            }
+            tflite.run(img, output);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        CameraLogger.setLogLevel(CameraLogger.LEVEL_VERBOSE);
+
+        camera = findViewById(R.id.camera);
+        camera.setLifecycleOwner(this);
+        camera.addCameraListener(new CameraListener() {
+            public void onCameraOpened(@NonNull CameraOptions options) { onOpened(options); }
+            public void onPictureTaken(@NonNull PictureResult result) { onPicture(result); }
+//            public void onVideoTaken(@NonNull VideoResult result) { onVideo(result); }
+            public void onCameraError(@NonNull CameraException exception) {
+                onError(exception);
+            }
+        });
+
+//        findViewById(R.id.edit).setOnClickListener(this);
+        findViewById(R.id.capturePicture).setOnClickListener(this);
+//        findViewById(R.id.capturePictureSnapshot).setOnClickListener(this);
+//        findViewById(R.id.captureVideo).setOnClickListener(this);
+//        findViewById(R.id.captureVideoSnapshot).setOnClickListener(this);
+        findViewById(R.id.toggleCamera).setOnClickListener(this);
+
+        controlPanel = findViewById(R.id.controls);
+        ViewGroup group = (ViewGroup) controlPanel.getChildAt(0);
+        Control[] controls = Control.values();
+        for (Control control : controls) {
+            ControlView view = new ControlView(this, control, this);
+            group.addView(view,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+
+        controlPanel.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                BottomSheetBehavior b = BottomSheetBehavior.from(controlPanel);
+                b.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
+
+        SizeSelector width = SizeSelectors.minWidth(200);
+        SizeSelector height = SizeSelectors.minHeight(200);
+        SizeSelector dimensions = SizeSelectors.and(width, height); // Matches sizes bigger than 1000x2000.
+        SizeSelector ratio = SizeSelectors.aspectRatio(AspectRatio.of(1, 1), 0); // Matches 1:1 sizes.
+
+        SizeSelector result = SizeSelectors.or(
+                SizeSelectors.and(ratio, dimensions), // Try to match both constraints
+                ratio, // If none is found, at least try to match the aspect ratio
+                SizeSelectors.biggest() // If none is found, take the biggest
+        );
+        camera.setPictureSize(result);
+        camera.setVideoSize(result);
+    }
+
+    private void message(String content, boolean important) {
+        int length = important ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT;
+        Toast.makeText(this, content, length).show();
+    }
+
+    private void onOpened(CameraOptions options) {
+        ViewGroup group = (ViewGroup) controlPanel.getChildAt(0);
+        for (int i = 0; i < group.getChildCount(); i++) {
+            ControlView view = (ControlView) group.getChildAt(i);
+            view.onCameraOpened(camera, options);
+        }
+    }
+
+    private void onError(@NonNull CameraException exception) {
+        message("Got CameraException #" + exception.getReason(), true);
+    }
+
+    private void onPicture(PictureResult result) {
+        if (camera.isTakingVideo()) {
+            message("Captured while taking video. Size=" + result.getSize(), false);
+            return;
+        }
+
+        // This can happen if picture was taken with a gesture.
+        long callbackTime = System.currentTimeMillis();
+        if (mCaptureTime == 0) mCaptureTime = callbackTime - 300;
+//        PicturePreviewActivity.setPictureResult(result);
+
+        float[][] netOutput = doInference(result);
+
+//        Intent intent = new Intent(CameraActivity.this, PicturePreviewActivity.class);
+        Intent intent = new Intent(CameraActivity.this, ResultsActivity.class);
+        intent.putExtra("delay", callbackTime - mCaptureTime);
+
+        float neutral = netOutput[0][0];
+        float happy = netOutput[0][1];
+        float sad = netOutput[0][2];
+        float surprise = netOutput[0][3];
+        float fear = netOutput[0][4];
+        float disgust = netOutput[0][5];
+        float angry = netOutput[0][6];
+
+        intent.putExtra("neutral", neutral);
+        intent.putExtra("happy", happy);
+        intent.putExtra("sad", sad);
+        intent.putExtra("surprise", surprise);
+        intent.putExtra("fear", fear);
+        intent.putExtra("disgust", disgust);
+        intent.putExtra("angry", angry);
+
+
+        startActivity(intent);
+        mCaptureTime = 0;
+    }
+
+//    private void onVideo(VideoResult video) {
+//        VideoPreviewActivity.setVideoResult(video);
+//        Intent intent = new Intent(CameraActivity.this, VideoPreviewActivity.class);
+//        startActivity(intent);
+//    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.edit: edit(); break;
+            case R.id.capturePicture: capturePicture(); break;
+//            case R.id.capturePictureSnapshot: capturePictureSnapshot(); break;
+//            case R.id.captureVideo: captureVideo(); break;
+//            case R.id.captureVideoSnapshot: captureVideoSnapshot(); break;
+            case R.id.toggleCamera: toggleCamera(); break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        BottomSheetBehavior b = BottomSheetBehavior.from(controlPanel);
+        if (b.getState() != BottomSheetBehavior.STATE_HIDDEN) {
+            b.setState(BottomSheetBehavior.STATE_HIDDEN);
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    private void edit() {
+        BottomSheetBehavior b = BottomSheetBehavior.from(controlPanel);
+        b.setState(BottomSheetBehavior.STATE_COLLAPSED);
+    }
+
+    private void capturePicture() {
+        if (camera.getMode() == Mode.VIDEO) {
+            message("Can't take HQ pictures while in VIDEO mode.", false);
+            return;
+        }
+        if (camera.isTakingPicture()) return;
+
+
+
+
+        mCaptureTime = System.currentTimeMillis();
+        message("Capturing picture...", false);
+        camera.takePicture();
+    }
+
+    private void capturePictureSnapshot() {
+        if (camera.isTakingPicture()) return;
+        mCaptureTime = System.currentTimeMillis();
+        message("Capturing picture snapshot...", false);
+        camera.takePictureSnapshot();
+    }
+
+    private void captureVideo() {
+        if (camera.getMode() == Mode.PICTURE) {
+            message("Can't record HQ videos while in PICTURE mode.", false);
+            return;
+        }
+        if (camera.isTakingPicture() || camera.isTakingVideo()) return;
+        message("Recording for 5 seconds...", true);
+        camera.takeVideo(new File(getFilesDir(), "video.mp4"), 5000);
+    }
+
+    private void captureVideoSnapshot() {
+        if (camera.isTakingVideo()) {
+            message("Already taking video.", false);
+            return;
+        }
+        message("Recording snapshot for 5 seconds...", true);
+        camera.takeVideoSnapshot(new File(getFilesDir(), "video.mp4"), 5000);
+    }
+
+    private void toggleCamera() {
+        if (camera.isTakingPicture() || camera.isTakingVideo()) return;
+        switch (camera.toggleFacing()) {
+            case BACK:
+                message("Switched to back camera!", false);
+                break;
+
+            case FRONT:
+                message("Switched to front camera!", false);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onValueChanged(Control control, Object value, String name) {
+        if (!camera.isHardwareAccelerated() && (control == Control.WIDTH || control == Control.HEIGHT)) {
+            if ((Integer) value > 0) {
+                message("This device does not support hardware acceleration. " +
+                        "In this case you can not change width or height. " +
+                        "The view will act as WRAP_CONTENT by default.", true);
+                return false;
+            }
+        }
+        control.applyValue(camera, value);
+        BottomSheetBehavior b = BottomSheetBehavior.from(controlPanel);
+        b.setState(BottomSheetBehavior.STATE_HIDDEN);
+        message("Changed " + control.getName() + " to " + name, false);
+        return true;
+    }
+
+    //region Permissions
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean valid = true;
+        for (int grantResult : grantResults) {
+            valid = valid && grantResult == PackageManager.PERMISSION_GRANTED;
+        }
+        if (valid && !camera.isOpened()) {
+            camera.open();
+        }
+    }
+
+    //endregion
+}
---
+++ app/src/main/java/com/example/emshnio/ResultsActivity.java
@@ -0,0 +1,114 @@
+package com.example.emshnio;
+
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        PieChart pieChart = findViewById(R.id.piechart);
+        pieChart.setUsePercentValues(true);
+        BarChart barChart = findViewById(R.id.barchart);
+
+        //List<PieEntry> value = new ArrayList<>();
+        List<BarEntry> entries= new ArrayList<>();
+
+        Bundle extras = getIntent().getExtras();
+
+        float neutral = extras.getFloat("neutral");
+        float happy = extras.getFloat("happy");
+        float sad = extras.getFloat("sad");
+        float surprise = extras.getFloat("surprise");
+        float fear = extras.getFloat("fear");
+        float disgust = extras.getFloat("disgust");
+        float anger = extras.getFloat("angry");
+
+        //value.add(new PieEntry(neutral, "Neutral"));
+        //value.add(new PieEntry(happy, "Happy"));
+        //value.add(new PieEntry(sad, "Sad"));
+        //value.add(new PieEntry(surprise, "Surprise"));
+        //value.add(new PieEntry(fear, "Fear"));
+        //value.add(new PieEntry(disgust, "Disgust"));
+        //value.add(new PieEntry(anger, "Anger"));
+
+        entries.add(new BarEntry(0f, neutral));
+        entries.add(new BarEntry(1f, happy));
+        entries.add(new BarEntry(2f, sad));
+        entries.add(new BarEntry(3f, surprise));
+        entries.add(new BarEntry(4f, fear));
+        entries.add(new BarEntry(5f, disgust));
+        entries.add(new BarEntry(6f, anger));
+
+        BarDataSet set = new BarDataSet(entries, "Emotion Distribution");
+        BarData data = new BarData(set);
+        data.setBarWidth(0.9f); // set custom bar width
+        barChart.setData(data);
+        barChart.setFitBars(true); // make the x-axis fit exactly all bars
+        set.setColors(ColorTemplate.JOYFUL_COLORS);
+        barChart.animateXY(1400,1400);
+        barChart.invalidate(); // refresh
+
+        // the labels that should be drawn on the XAxis
+        final String[] quarters = new String[] { "Neutral", "Happy", "Sad", "Surprise","Fear","Disgust","Anger" };
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new MyXAxisValueFormatter(quarters));
+        xAxis.setGranularity(1); // minimum axis-step (interval) is 1
+        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+
+        //PieDataSet pieDataSet = new PieDataSet(value, "Emotion Distribution");
+        //PieData pieData = new PieData(pieDataSet);
+        //pieChart.setData(pieData);
+        //pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        //pieChart.animateXY(1400, 1400);
+
+    }
+
+    class MyXAxisValueFormatter implements IAxisValueFormatter {
+
+        private String[] mValues;
+
+        public MyXAxisValueFormatter(String[] values) {
+            this.mValues = values;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            // "value" represents the position of the label on the axis (x or y)
+            return mValues[(int) value];
+        }
+    }
+
+}
