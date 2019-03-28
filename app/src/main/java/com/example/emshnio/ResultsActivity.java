8e3935318862918e925b8cfe92f0db7a7fdce191
dingpiano <alexander.yding@gmail.com>
2019-03-28 11:49:44 -0400

    bar chart only displays top 3 emotions

--- app/src/main/java/com/example/emshnio/ResultsActivity.java
+++ app/src/main/java/com/example/emshnio/ResultsActivity.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
@@ -16,6 +15,7 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -28,15 +28,6 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -59,27 +50,51 @@ public class ResultsActivity extends AppCompatActivity {
         float disgust = extras.getFloat("disgust");
         float anger = extras.getFloat("angry");
 
-        //value.add(new PieEntry(neutral, "Neutral"));
-        //value.add(new PieEntry(happy, "Happy"));
-        //value.add(new PieEntry(sad, "Sad"));
-        //value.add(new PieEntry(surprise, "Surprise"));
-        //value.add(new PieEntry(fear, "Fear"));
-        //value.add(new PieEntry(disgust, "Disgust"));
-        //value.add(new PieEntry(anger, "Anger"));
+        float values[] = new float[]{neutral, happy, sad, surprise, fear, disgust, anger};
+        final String[] emotions = new String[]{"Neutral", "Happy", "Sad", "Surprise","Fear","Disgust","Anger"};
+        int max1_index = 0;
+        int max2_index = 0;
+        int max3_index = 0;
+        float maxValue1 = 0;
+        float maxValue2 = 0;
+        float maxValue3 = 0;
 
-        entries.add(new BarEntry(0f, neutral));
-        entries.add(new BarEntry(1f, happy));
-        entries.add(new BarEntry(2f, sad));
-        entries.add(new BarEntry(3f, surprise));
-        entries.add(new BarEntry(4f, fear));
-        entries.add(new BarEntry(5f, disgust));
-        entries.add(new BarEntry(6f, anger));
+        for(int i=0;i < values.length;i++) {
+            if (values[i] > maxValue1) {
+                maxValue1 = values[i];
+                max1_index = i;
+            }
+        }
+        for(int i=0;i < values.length;i++) {
+            if (values[i] > maxValue2 && values[i] < maxValue1) {
+                maxValue2 = values[i];
+                max2_index = i;
+            }
+        }
+        for(int i=0;i < values.length;i++) {
+            if (values[i] > maxValue3 && values[i] < maxValue2) {
+                maxValue3 = values[i];
+                max3_index = i;
+            }
+        }
+
+        entries.add(new BarEntry(0f, maxValue1));
+        entries.add(new BarEntry(1f, maxValue2));
+        entries.add(new BarEntry(2f, maxValue2));
+
+        //entries.add(new BarEntry(0f, neutral));
+        //entries.add(new BarEntry(1f, happy));
+        //entries.add(new BarEntry(2f, sad));
+        //entries.add(new BarEntry(3f, surprise));
+        //entries.add(new BarEntry(4f, fear));
+        //entries.add(new BarEntry(5f, disgust));
+        //entries.add(new BarEntry(6f, anger));
 
         BarDataSet set = new BarDataSet(entries, "Emotion Distribution");
         //set.setColors(new int[] {Color.GRAY, Color.YELLOW, Color.GREEN, Color.MAGENTA, Color.BLUE, Color.MAGENTA, Color.DKGRAY});
         BarData data = new BarData(set);
         data.setBarWidth(0.9f); // set custom bar width
-        data.setValueTextSize(15f);
+        data.setValueTextSize(17f);
         barChart.setData(data);
         barChart.setFitBars(true); // make the x-axis fit exactly all bars
         barChart.setDrawValueAboveBar(false);
@@ -91,21 +106,16 @@ public class ResultsActivity extends AppCompatActivity {
         legend.setEnabled(false);
 
         // the labels that should be drawn on the XAxis
-        final String[] quarters = new String[] { "Neutral", "Happy", "Sad", "Surprise","Fear","Disgust","Anger" };
+        //final String[] quarters = new String[] { "Neutral", "Happy", "Sad", "Surprise","Fear","Disgust","Anger" };
+        final String[] quarters = new String[] {emotions[max1_index], emotions[max2_index], emotions[max3_index]};
 
         XAxis xAxis = barChart.getXAxis();
         xAxis.setValueFormatter(new MyXAxisValueFormatter(quarters));
         xAxis.setGranularity(1); // minimum axis-step (interval) is 1
         xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
-        xAxis.setTextSize(14.5f);
+        xAxis.setTextSize(16f);
         xAxis.setDrawAxisLine(true);
         xAxis.setDrawGridLines(false);
-
-        //PieDataSet pieDataSet = new PieDataSet(value, "Emotion Distribution");
-        //PieData pieData = new PieData(pieDataSet);
-        //pieChart.setData(pieData);
-        //pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
-        //pieChart.animateXY(1400, 1400);
 
     }
 
