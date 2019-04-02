8f80dfcb26c4846645e25e14f3c839301f61396e
Balu Nathan <balu.nathan.04@gmail.com>
2019-04-02 06:48:13 -0400

    Added menu page.

---
+++ app/src/main/java/com/example/emshnio/MenuActivity.java
@@ -0,0 +1,45 @@
+package com.example.emshnio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.goToSingleCaptureButton: goToSingleCapture(); break;
+            case R.id.goToStreamCaptureButton: goToStreamCapture(); break;
+            case R.id.goToHelpButton: goToHelp(); break;
+        }
+    }
+
+    private void goToHelp() {
+
+        Intent intent = new Intent(MenuActivity.this, HelpActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToStreamCapture() {
+
+        Intent intent = new Intent(MenuActivity.this, CameraActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void goToSingleCapture() {
+
+        Intent intent = new Intent(MenuActivity.this, CameraActivity.class);
+        startActivity(intent);
+
+    }
+}
